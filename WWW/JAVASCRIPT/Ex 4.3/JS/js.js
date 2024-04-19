50fcb550c06e4a24d5878c5c2bbb9cf797f1df17
Niki7 <121088976+NiKi4856853938578467@users.noreply.github.com>
2024-04-19 12:42:05 +0200

    Add files via upload

---
+++ WWW/JAVASCRIPT/Ex 4.3/JS/js.js
@@ -0,0 +1,41 @@
+function myfunction(){
+
+    let input1 = document.getElementById("input").value
+
+
+    if (input1 < 5) {
+     resultado = " Suspés ";
+    }
+    else if (input1 < 6) {
+        resultado = " Suficient ";
+    }
+    else if (input1 < 7) {
+        resultado = " Bé "
+    }
+    else if (input1 < 9) {
+        resultado = " Notable "
+    }
+    else if (input1 <= 10) {
+        resultado = " Exel·lent "
+    }
+        document.getElementById("resultado").innerHTML = resultado;
+    }
+    
+    
+    
+    
+        
+    
+      
+
+        
+        
+        
+        
+        
+            
+
+
+
+
+
